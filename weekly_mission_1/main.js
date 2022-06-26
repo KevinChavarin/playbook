5adc150bcfeff2c0f3774b799daaf0efa64aed14
Kevin Chavarin <Kevinke1177@gmail.com>
2022-06-25 20:35:36 -0500

    Modificando main.js

---
+++ weekly_mission_1/main.js
@@ -0,0 +1,7 @@
+console.log("Hola mundo, soy un explorer!");
+console.log("Hola mundo, soy un explorer!");
+console.log("Hola mundo, soy un explorer!");
+console.log("Hola mundo, soy un explorer!");
+console.log("Hola mundo, soy un programador!");
+
+console.log("Me gustan las tortugas");
