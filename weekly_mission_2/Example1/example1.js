3ce5b84793db47d8bf59f00d37accac5b981ac50
Kevin Chavarin <Kevinke1177@gmail.com>
2022-08-19 21:37:21 -0500

    Poniendo bonito el código

--- weekly_mission_2/Example1/example1.js
+++ weekly_mission_2/Example1/example1.js
@@ -1,87 +1,77 @@
 console.log("Examples 1");
 
-const names=["Carlo", "Juan", "Pedro", "Pablo", "Jorge"];
+const names = ["Carlo", "Juan", "Pedro", "Pablo", "Jorge"];
 console.log(names);
 
-const explorers=[
-    {
-        name: "Explorer 1",
-        exercises_completed: 10,
-        rate: 99,
-        city: "CDMX",
-        stack: [
-            "js",
-            "c#",
-        ],
-        missions: {
-            onboarding: {
-                isFinished: true,
-                exercisesFinished: true,
-
-            }
-        }
-
+const explorers = [
+  {
+    name: "Explorer 1",
+    exercises_completed: 10,
+    rate: 99,
+    city: "CDMX",
+    stack: ["js", "c#"],
+    missions: {
+      onboarding: {
+        isFinished: true,
+        exercisesFinished: true,
+      },
     },
-    {
-        name: "Explorer 2",
-        exercises_completed: 9,
-        rate: 50,
-        city: "Veracruz",
-        stack: [
-            "js"
-        ],
-        missions: {
-            onboarding: {
-                isFinished: false,
-                exercisesFinished: false,
-            },
-            frontend:{
-                isFinished: false,
-                exercisesFinished: false
-            }
-        }
-
+  },
+  {
+    name: "Explorer 2",
+    exercises_completed: 9,
+    rate: 50,
+    city: "Veracruz",
+    stack: ["js"],
+    missions: {
+      onboarding: {
+        isFinished: false,
+        exercisesFinished: false,
+      },
+      frontend: {
+        isFinished: false,
+        exercisesFinished: false,
+      },
     },
-    {
-        name: "Explorer 3",
-        exercises_completed: 3,
-        rate: 100,
-        city: "Sonora",
-        stack: [
-            "elixir"
-        ],
-        missions: {
-            onboarding: {
-                isFinished: true,
-                exercisesFinished: true,
-
-            }
-        }
-
+  },
+  {
+    name: "Explorer 3",
+    exercises_completed: 3,
+    rate: 100,
+    city: "Sonora",
+    stack: ["elixir"],
+    missions: {
+      onboarding: {
+        isFinished: true,
+        exercisesFinished: true,
+      },
     },
-]
+  },
+];
 
-console.log(explorers)
+console.log(explorers);
 
 console.log("Ejercicio 2.1");
-explorers.forEach(explorer => console.log(explorer.name))
+explorers.forEach((explorer) => console.log(explorer.name));
 
 console.log("Ejercicio 2.2");
-explorers.forEach(item=>console.log(item.stack));
+explorers.forEach((item) => console.log(item.stack));
 
 console.log("Ejercicio 2.3");
-const explorers_stacks=explorers.map(item=>item.stack);
+const explorers_stacks = explorers.map((item) => item.stack);
 console.log(explorers_stacks);
 
 console.log("Ejercicio 2.4");
-const explorer_with_js=explorers.filter(item=>item.stack.includes("js"));
+const explorer_with_js = explorers.filter((item) => item.stack.includes("js"));
 console.log(explorer_with_js);
 
 console.log("Ejercicio 2.5");
-const first_explorer_in_cdmx=explorers.find(item=>item.city==="CDMX");
+const first_explorer_in_cdmx = explorers.find((item) => item.city === "CDMX");
 console.log(first_explorer_in_cdmx);
 
 console.log("Ejercicio 2.6");
-const all_exercises=explorers.reduce((acc, item)=> acc + item.exercises_completed, 0);
+const all_exercises = explorers.reduce(
+  (acc, item) => acc + item.exercises_completed,
+  0
+);
 console.log(all_exercises);
-
